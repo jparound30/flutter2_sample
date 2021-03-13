ca18135d0700538e9f1d1944a2686c3c12c7f88f
Nobuhiro Tabuki <japanese.around30@gmail.com>
2021-03-13 17:52:23 +0900

    trivial fix

--- lib/provider/credential_info.dart
+++ lib/provider/credential_info.dart
@@ -4,6 +4,10 @@ class CredentialInfo with ChangeNotifier {
   String? _space;
   String? _apiKey;
 
+  CredentialInfo({String? space, String? apiKey})
+      : _space = space,
+        _apiKey = apiKey;
+
   String? get space => _space;
 
   String? get apiKey => _apiKey;
