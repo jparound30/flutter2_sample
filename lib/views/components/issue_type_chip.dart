62df147f62a3b4d5bbe9253a851b757ef5c5a0ea
jparound30 <japanese.around30@gmail.com>
2021-03-20 22:19:29 +0900

    add issue type chip component

---
+++ lib/views/components/issue_type_chip.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter2_sample/models/issue.dart';
+
+import '../../color_converter.dart';
+
+class IssueTypeChip extends StatelessWidget {
+  final IssueType issueType;
+
+  IssueTypeChip(this.issueType);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+      decoration: ShapeDecoration(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(8)),
+        ),
+        color: ColorConverter.fromString(issueType.color),
+      ),
+      child: Text(
+        issueType.name,
+        style: TextStyle(
+          fontSize: 12.0,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
