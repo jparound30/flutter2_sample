84a1721e275a5d89101766dc645b86ea1749b127
Nobuhiro Tabuki <japanese.around30@gmail.com>
2021-03-20 12:07:47 +0900

    WIP issue detail

--- lib/views/screens/issue_detail_page.dart
+++ lib/views/screens/issue_detail_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:intl/intl.dart';
 
 import '../../models/issue.dart';
 
@@ -15,14 +16,109 @@ class IssueDetailPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          _issue.summary,
+          _issue.issueKey,
           maxLines: 1,
           overflow: TextOverflow.ellipsis,
         ),
         actions: [],
         centerTitle: false,
       ),
-      body: Text(_issue.description),
+      body: IssueDetail(
+        issue: _issue,
+      ),
+    );
+  }
+}
+
+class IssueDetail extends StatelessWidget {
+  final Issue issue;
+
+  IssueDetail({required this.issue});
+
+  @override
+  Widget build(BuildContext context) {
+    var dateFormatYmd = DateFormat('yyyy/MM/dd', 'ja');
+
+    var startDate = dateFormatYmd.format(issue.startDate!);
+    var dueDate = issue.dueDate != null ? dateFormatYmd.format(issue.dueDate!) : "未設定";
+
+    return Container(
+      constraints: BoxConstraints.expand(),
+      child: Scrollbar(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(8.0),
+          scrollDirection: Axis.vertical,
+          child: Column(
+            children: [
+              Text(issue.issueType.name), // TODO カラーなど
+              Text(issue.createdUser.name), // TODO
+              Text(startDate), // TODO
+              Text(dueDate), // TODO
+              Text(issue.status.name), // TODO カラーなど
+              Container(
+                width: double.infinity,
+                child: Card(
+                  child: Text(this.issue.summary),
+                ),
+              ),
+              BacklogMarkdownRender(markdown: issue.description),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Expanded(child: Text(issue.priority.name)),
+                  Expanded(
+                      child: Text(
+                          issue.assignee != null ? issue.assignee!.name : ""))
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Expanded(child: Text("TODO カテゴリ")), // TODO
+                  Expanded(child: Text("TODO マイルストーン")), // TODO
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Expanded(child: Text("TODO 発生バージョン")), // TODO
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Expanded(child: Text(issue.estimatedHours.toString())), // TODO
+                  Expanded(child: Text(issue.actualHours.toString())), // TODO
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Expanded(child: Text("TODO 完了理由")), // TODO
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class BacklogMarkdownRender extends StatelessWidget {
+  final String markdown;
+
+  BacklogMarkdownRender({
+    required this.markdown,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: Card(
+        child: Text(this.markdown),
+      ),
     );
   }
 }
