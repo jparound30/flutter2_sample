8ae8c6451ffa64c73139ec45535682be509a8f8d
Nobuhiro Tabuki <japanese.around30@gmail.com>
2021-03-21 15:17:08 +0900

    add category model

--- lib/models/issue.dart
+++ lib/models/issue.dart
@@ -1,5 +1,25 @@
 import 'user.dart';
 
+class Category {
+  final int id;
+  final String name;
+  final int displayOrder;
+
+  Category({
+    required this.id,
+    required this.name,
+    required this.displayOrder,
+  });
+
+  factory Category.fromJson(Map<String, dynamic> json) {
+    return Category(
+      id: json['id'],
+      name: json['name'],
+      displayOrder: json['displayOrder'],
+    );
+  }
+}
+
 class MilestoneVersion {
   final int id;
   final int projectId;
