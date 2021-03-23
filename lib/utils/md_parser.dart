423757558dd9f27e54c3b3d5b9f2c3f16def896f
jparound30 <japanese.around30@gmail.com>
2021-03-23 22:45:58 +0900

    WIP implement backlog markdown parser
    unordered list

--- lib/utils/md_parser.dart
+++ lib/utils/md_parser.dart
@@ -1,10 +1,22 @@
 class MdElement {}
 
+/// 見出し
 class MdTitle extends MdElement {
   final int level;
   final String content;
 
   MdTitle({
+    required this.level,
+    required this.content,
+  });
+}
+
+/// 箇条書き
+class MdUnorderedList extends MdElement {
+  final int level;
+  final String content;
+
+  MdUnorderedList({
     required this.level,
     required this.content,
   });
@@ -25,6 +37,19 @@ class MdParser {
         if (s.startsWith(' ')) {
           result.add(MdTitle(level: level, content: s.substring(1)));
         }
+        return;
+      }
+      // 箇条書き
+      if (line.startsWith('-')) {
+        var level = 1;
+        var s = "";
+        for (s = line.substring(level);
+        s.startsWith('-');
+        level++, s = s.substring(1)) {}
+        if (s.startsWith(' ')) {
+          result.add(MdUnorderedList(level: level, content: s.substring(1)));
+        }
+        return;
       }
     });
     return result;
--- test/backlog_markdown_parser_test.dart
+++ test/backlog_markdown_parser_test.dart
@@ -3,7 +3,7 @@ import 'package:flutter2_sample/utils/md_parser.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  final normal = '''
+  final title = '''
 * 見出し1
 ** 見出し2
 *** 見出し3
@@ -11,7 +11,7 @@ void main() {
   test(
     '見出し',
     () {
-      var ret = MdParser.parse(normal);
+      var ret = MdParser.parse(title);
       expect(ret.length, 3);
       expect(ret[0] is MdTitle, true);
       expect(ret[1] is MdTitle, true);
@@ -24,4 +24,31 @@ void main() {
       expect((ret[2] as MdTitle).content, "見出し3");
     },
   );
+
+  final unorderedList = '''
+- 箇条書き1
+- 箇条書き2
+-- 箇条書き2-1
+--- 箇条書き2-1-1
+''';
+  test(
+    '見出し',
+        () {
+      var ret = MdParser.parse(unorderedList);
+      expect(ret.length, 4);
+      expect(ret[0] is MdUnorderedList, true);
+      expect(ret[1] is MdUnorderedList, true);
+      expect(ret[2] is MdUnorderedList, true);
+      expect(ret[3] is MdUnorderedList, true);
+      expect((ret[0] as MdUnorderedList).level, 1);
+      expect((ret[0] as MdUnorderedList).content, "箇条書き1");
+      expect((ret[1] as MdUnorderedList).level, 1);
+      expect((ret[1] as MdUnorderedList).content, "箇条書き2");
+      expect((ret[2] as MdUnorderedList).level, 2);
+      expect((ret[2] as MdUnorderedList).content, "箇条書き2-1");
+      expect((ret[3] as MdUnorderedList).level, 3);
+      expect((ret[3] as MdUnorderedList).content, "箇条書き2-1-1");
+    },
+  );
+
 }
