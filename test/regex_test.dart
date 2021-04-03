d3418413ac39128d9482cbb155dec92961f39c77
jparound30 <japanese.around30@gmail.com>
2021-04-03 12:03:32 +0900

    WIP implement backlog markdown parser
    add regexp test

--- test/regex_test.dart
+++ test/regex_test.dart
@@ -11,6 +11,26 @@ void main() {
     expect(tableRowExp.hasMatch(ng1), false);
     expect(tableRowExp.hasMatch(ok1), true);
     expect(tableRowExp.hasMatch(ok2), true);
+  });
 
+  test('image', () {
+    final imageExp = new RegExp(r'#image\((?<name>[^()]+?)\)');
+
+    // final ng1 = "aaaaa";
+    final ok1 =
+        "この課題の問題点#image(スクリーンショット 2021-03-26 19.30.01.png) ppppptruyvjygjytvkjhgouigliyglygljhgjkhbljhjhbljhbljhblkjh;kjhkjhkljh";
+    final ok2 =
+        "この課題の問題点#image(data1.png) ppppptruyvjygjytvkjhgouigliyglygljhgjkhbljhjhbljhbljhblkjh;#image(data2.png)kjhkjhkljh";
+    // final ok2 = "| ホスト名 | IPアドレス   |     備考      |h";
+
+    // expect(tableRowExp.hasMatch(ng1), false);
+    expect(imageExp.hasMatch(ok1), true);
+    expect(imageExp.firstMatch(ok1)?.namedGroup("name"),
+        "スクリーンショット 2021-03-26 19.30.01.png");
+
+    var allMatches = imageExp.allMatches(ok2);
+    expect(allMatches.length, 2);
+    expect(allMatches.elementAt(0).namedGroup("name"), "data1.png");
+    expect(allMatches.elementAt(1).namedGroup("name"), "data2.png");
   });
 }
